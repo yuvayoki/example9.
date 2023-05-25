e552fb37095defdb9ca0b94eca345ac4a8fdae04
yuvayoki <121660458+yuvayoki@users.noreply.github.com>
2023-05-26 00:36:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = '';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('nav ul a');
+
+window.onscroll = () =>{
+    section.forEach(sec => {
+     let top = window.scrollY;
+     let offset =sec.offsetTop -150;
+     let height = sec.offsetHeight;
+     let id = sec.getAttribute('id');
+
+     if(top >= offset && top < offset + height){
+        navlinks.forEach(links =>{
+            links.classList.remove('active');
+            document.querySelector('nav ul a[href*='+ id +']').classList.add
+            ('active');
+        
+        });
+     };
+    });
+};
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
